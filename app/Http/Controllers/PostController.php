efa0428d2dc73acacb336db7a98c43e49f8ac085
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2022-08-17 14:53:28 +0900

    add Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -16,6 +17,18 @@ class PostController extends Controller
     public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);  
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+        
+    public function create()
+    {
+        return view('posts/create');
     }
     
     /**
