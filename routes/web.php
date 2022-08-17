7abeed16f94bedbb30fc19a0166e2879ad5c85ca
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2022-08-17 15:19:51 +0900

    add Edit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::post('/posts', 'PostController@store');
 Route::get('/posts/create', 'PostController@create');
 
 Route::get('/posts/{post}', 'PostController@show');
+
+Route::get('/posts/{post}/edit', 'PostController@edit');
+
+Route::put('/posts/{post}', 'PostController@update');
