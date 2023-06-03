e4bd5346ac8ac89cfce6b1983a09a8eb4ebd2b24
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-03 12:50:04 +0900

    comment Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
+// Post一覧を表示
+
+
 class PostController extends Controller
 {
     public function index(Post $post)
