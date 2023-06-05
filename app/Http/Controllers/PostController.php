caab60138a17f56083b1235f23c23e72d8219adb
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-05 18:40:03 +0900

    バリデーション設定

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 // Post一覧を表示
-
-
 class PostController extends Controller
 {
     public function index(Post $post)
@@ -19,5 +18,17 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->create($input);
+        return redirect('/posts/'.$post->id);
     }
 }
