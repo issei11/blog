05d29bcd50887d014b5eb19d25c8c274484c8be3
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-05 22:01:09 +0900

    投稿削除実装

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use DateTime;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory()->count(10)->create();
+    }
+}
